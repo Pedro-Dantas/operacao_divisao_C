52642c08f1cd94b5d9e2b46dd63caf5da60e9f2b
Pedro Dantas <dev.pedrodantas@gmail.com>
2017-03-01 18:07:21 -0300

    commit inicial operacao divisao c

---
+++ oper_div/maindiv.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Estudo em C, basta trocar o sinal para testar outras operaçoes 
+
+int main(int argc, char *argv[]) {
+	
+	float num1;
+	float num2;
+	float oper;
+	
+	printf("\nOPERACAO DE DIVISAO\n");
+	
+	printf("\nDigite um numero real: ");
+	scanf("%f", &num1);
+	
+	printf("Digite outro numero real: ");
+	scanf("%f", &num2);
+	
+	oper=num1/num2;
+	
+	printf("\nO resultado da operacao e: %f\n", oper);
+	
+	return 0;
+}
